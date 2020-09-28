16bb46b644e8cd3569c3bd11095dc67ef0c63258
val.love <vallove89@outlook.com>
2020-09-27 18:20:04 -0700

    Setup authentication

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Header from '../../components/Header';
+import { LoginPage } from '../../components/LoginPage';
 
 test('should render Header correctly', () => {
-  const wrapper = shallow(<Header />);
+  const wrapper = shallow(<Header startLogout={() => {}}/>);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogin on button click', () => {
+  const startLogin = jest.fn();
+  const wrapper = shallow(<LoginPage startLogin={startLogin} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogin).toHaveBeenCalled();
 });
